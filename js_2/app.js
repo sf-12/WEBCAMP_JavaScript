816dac6dea02c482bd5394e32452662471b0460b
sf-12 <s.fukasawa12@gmail.com>
2020-11-13 19:39:49 +0900

    learned basic grammer

--- js_2/app.js
+++ js_2/app.js
@@ -1,1 +1,7 @@
-alert('Hello World');
+var num1 = 10;
+var num2 = 5;
+
+alert('num1 + num2 = ' + (num1 + num2) );
+alert('num1 - num2 = ' + (num1 - num2) );
+alert('num1 * num2 = ' + (num1 * num2) );
+alert('num1 ÷ num2 = ' + (num1 / num2) );
